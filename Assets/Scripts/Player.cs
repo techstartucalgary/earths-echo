87c528535852965181bb67c352371aa8dd97fa46
lujarios <luca.jrios@gmail.com>
2025-01-16 20:18:49 -0700

    Squashed commit of the following:
    commit 3bdaece6e720e6e5c35e3bd8c620936e934da0c8
    Author: lujarios <luca.jrios@gmail.com>
    Date:   Thu Jan 16 20:10:57 2025 -0700
    
        Moving things round
    
    commit 37409c1ef5e5cc87f07bb8d3b35b10f54de810af
    Author: lujarios <luca.jrios@gmail.com>
    Date:   Thu Jan 16 19:39:52 2025 -0700
    
        Minor Changes
    
    commit 06e900dccd14441d04f3739e46b47d711f53e31b
    Author: lujarios <luca.jrios@gmail.com>
    Date:   Thu Jan 16 19:39:11 2025 -0700
    
        Minor Movement Changes
    
    commit 4ef086e188b0193bfc806230d966a8473aee8ac1
    Author: lujarios <luca.jrios@gmail.com>
    Date:   Wed Jan 15 21:09:01 2025 -0700
    
        Update Scene.unity
    
    commit f44af1c469afb245fc33f4f799aa8c3f66db61b6
    Author: lujarios <luca.jrios@gmail.com>
    Date:   Wed Nov 20 01:21:41 2024 -0700
    
        Some fixes
    
    commit cdc0b31402405c2cc63e7e8265c2cd849bbe8b70
    Author: lujarios <luca.jrios@gmail.com>
    Date:   Mon Nov 18 08:57:29 2024 -0700
    
        Ladder and Platforms
    
    commit 8c3d3c9dd44cab7a97ad209c466bf4f2b30becba
    Author: lujarios <luca.jrios@gmail.com>
    Date:   Sun Oct 20 17:54:29 2024 -0600
    
        Sliding, Sprinting, Crawling
    
        Shift to sprint
        C to slide while sprinting
        Hold C to crawl
        Jump while sliding to slide jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,81 +1,101 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.SceneManagement;
 
 [RequireComponent (typeof (Controller2D))]
+[RequireComponent(typeof(BoxCollider2D))]
 public class Player : MonoBehaviour {
 
-    public HealthBar healthBar;
-
+    // Jumping variables
     public float maxJumpHeight = 4;
-	public float minJumpHeight = 1;
-	public float timeToJumpApex = .4f;
-	float accelerationTimeAirborne = .2f;
-	float accelerationTimeGrounded = .1f;
-    [SerializeField] float moveSpeed = 6;
+    public float minJumpHeight = 1;
+    public float timeToJumpApex = .4f;
+    float accelerationTimeAirborne = .2f;
+    float accelerationTimeGrounded = .1f;
+    float gravity;
+    float maxJumpVelocity;
+    float minJumpVelocity;
 
-	public Vector2 wallJumpClimb;
-	public Vector2 wallJumpOff;
-	public Vector2 wallLeap;
+    // Movement variables
+    Vector2 directionalInput;
+    float moveSpeed = 6;
+    public Vector3 velocity;
+    float velocityXSmoothing;
+    [SerializeField] public float playerSpeed;
+    [SerializeField] private float sprintMultiplier = 1.5f;  // Speed multiplier when sprinting
+    [SerializeField] private float sprintRampUpTime = 1f;    // Time to reach full sprint speed
+    private float currentSprintTime = 0f;                    // Tracks ramp-up progress
+    public bool isSprinting = false;
 
-	public float wallSlideSpeedMax = 3;
-	public float wallStickTime = .25f;
-	float timeToWallUnstick;
+    // Sliding variables
+    [SerializeField] private float slideMultiplier = 0.3f;            // Speed boost during slide
+    [SerializeField] private float slideRampDownTime = 2f;    // Time to reach lowest slide speed
+    private float currentSlideTime = 0f;
+    [SerializeField] private float slideJumpBoost = 1.5f;       // Horizontal boost when slide jumping
+    [SerializeField] private float crawlSpeed = 2f;             // Slow speed during crawling
+    [SerializeField] private float slideCooldown = 0.5f;        // Time between slide jumps
+    [SerializeField] private float slideShrinkFactor = 0.5f;    // Factor to shrink player when sliding
+    public bool isSliding = false;
+    private bool isCrawling = false;
+    private Vector3 originalScale;                              // Store the original player scale
 
-	float gravity;
-	float maxJumpVelocity;
-	float minJumpVelocity;
-	Vector3 velocity;
-	float velocityXSmoothing;
+    // Wall interaction variables
+    public Vector2 wallJumpClimb;
+    public Vector2 wallJumpOff;
+    public Vector2 wallLeap;
+    public float wallSlideSpeedMax = 3;
+    public float wallStickTime = .25f;
+    float timeToWallUnstick;
+    bool wallSliding;
+    int wallDirX;
 
-	Controller2D controller;
+    // References
+    Controller2D controller;
+    BoxCollider2D boxCollider;
+    // public HealthBar healthBar;
 
-	Vector2 directionalInput;
-	bool wallSliding;
-	int wallDirX;
+    // Ladder stuff
+    public GameObject touchingLadder = null;
+    public bool canClimb = false;
+    public bool climbing = false;
+    public float climbSpeed = 5f;
+    public float dismountTime = 1.5f;
+    public float timeToReclimb = 0f;
 
-	void Start() {
+    void Start() {
 		controller = GetComponent<Controller2D> ();
+        boxCollider = GetComponent<BoxCollider2D> ();
 
 		gravity = -(2 * maxJumpHeight) / Mathf.Pow (timeToJumpApex, 2);
 		maxJumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
 		minJumpVelocity = Mathf.Sqrt (2 * Mathf.Abs (gravity) * minJumpHeight);
-	}
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.tag == "FallDetector")
-        {
-        }
-        else if (collision.tag == "Checkpoint")
-        {
-        }
-        else if (collision.tag == "NextLevel")
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-            // Can also use SceneManager.LoadScene(1); to load a specific scene
-        }
-        else if (collision.tag == "PreviousLevel")
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-        }
-        else if (collision.tag == "Crystal")
-        {
-            collision.gameObject.SetActive(false);
-        }
+
+        originalScale = transform.localScale; // Save original size for scaling during slide
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.tag == "spike")
+	void Update() {
+		
+        if (canClimb && directionalInput.y > 0 && timeToReclimb <= 0)
         {
-            healthBar.Damage(0.002f);
+            climbing = true;
+            AlignToLadder();
         }
-    }
-    void Update() {
-		CalculateVelocity ();
-		HandleWallSliding ();
+        else if (timeToReclimb > 0)
+        {
+            timeToReclimb -= Time.deltaTime;
+        }
 
-		controller.Move (velocity * Time.deltaTime, directionalInput);
+        if (climbing)
+        {
+            ClimbLadder();
+        }
+        else
+        {
+            CalculateVelocity();
+        }
+
+        HandleWallSliding();
+
+        controller.Move (velocity * Time.deltaTime, directionalInput);
 
 		if (controller.collisions.above || controller.collisions.below) {
 			if (controller.collisions.slidingDownMaxSlope) {
@@ -84,45 +104,126 @@ public class Player : MonoBehaviour {
 				velocity.y = 0;
 			}
 		}
-	}
 
-	public void SetDirectionalInput (Vector2 input) {
+        HandleSprinting();
+        HandleSliding();
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ladder"))
+        {
+            touchingLadder = collision.gameObject; // Save the ladder object
+            canClimb = true;                      // Set climbing state to true
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        // Check if the player leaves a ladder
+        if (collision.CompareTag("Ladder"))
+        {
+            touchingLadder = null; // Clear the ladder reference
+            canClimb = false;     // Set climbing state to false
+        }
+    }
+
+    public void AlignToLadder()
+    {
+        if (touchingLadder != null)
+        {
+            Vector3 ladderPosition = touchingLadder.transform.position;
+
+            // Align the player's X position to the ladder's X position
+            Vector3 newPlayerPosition = transform.position;
+            newPlayerPosition.x = ladderPosition.x;
+            transform.position = newPlayerPosition;
+        }
+    }
+
+    public void ClimbLadder()
+    {
+        BoxCollider2D ladderCollider = touchingLadder.GetComponent<BoxCollider2D>();
+        float ladderTop = ladderCollider.bounds.max.y; // Top of the ladder
+        float ladderBottom = ladderCollider.bounds.min.y; // Bottom of the ladder
+
+        BoxCollider2D playerCollider = GetComponent<BoxCollider2D>();
+        float playerTop = playerCollider.bounds.max.y; // Top of the player
+        float playerBottom = playerCollider.bounds.min.y; // Bottom of the player
+
+        velocity.x = 0;
+        velocity.y = directionalInput.y * climbSpeed;
+
+        if (directionalInput.y == 0 || 
+           (playerBottom >= ladderTop && directionalInput.y > 0) || 
+           (playerTop <= ladderBottom && directionalInput.y < 0))
+        {
+            velocity.y = 0;
+        }
+    }
+
+    public void SetDirectionalInput (Vector2 input) {
 		directionalInput = input;
 	}
 
 	public void OnJumpInputDown() {
-		if (wallSliding) {
-			if (wallDirX == directionalInput.x) {
-				velocity.x = -wallDirX * wallJumpClimb.x;
-				velocity.y = wallJumpClimb.y;
-			}
-			else if (directionalInput.x == 0) {
-				velocity.x = -wallDirX * wallJumpOff.x;
-				velocity.y = wallJumpOff.y;
-			}
-			else {
-				velocity.x = -wallDirX * wallLeap.x;
-				velocity.y = wallLeap.y;
-			}
-		}
-		if (controller.collisions.below) {
-			if (controller.collisions.slidingDownMaxSlope) {
-				if (directionalInput.x != -Mathf.Sign (controller.collisions.slopeNormal.x)) { // not jumping against max slope
-					velocity.y = maxJumpVelocity * controller.collisions.slopeNormal.y;
-					velocity.x = maxJumpVelocity * controller.collisions.slopeNormal.x;
-				}
-			} else {
-				velocity.y = maxJumpVelocity;
-			}
-		}
+
+        bool wasClimbing = false;
+        if (climbing)
+        {
+            climbing = false;
+            wasClimbing = true;
+            timeToReclimb = dismountTime;
+        }
+
+        if (wallSliding)
+        {
+            if (wallDirX == directionalInput.x)
+            {
+                velocity.x = -wallDirX * wallJumpClimb.x;
+                velocity.y = wallJumpClimb.y;
+            }
+            else if (directionalInput.x == 0)
+            {
+                velocity.x = -wallDirX * wallJumpOff.x;
+                velocity.y = wallJumpOff.y;
+            }
+            else
+            {
+                velocity.x = -wallDirX * wallLeap.x;
+                velocity.y = wallLeap.y;
+            }
+        }
+        if (controller.collisions.below || wasClimbing)
+        {
+            if (controller.collisions.slidingDownMaxSlope)
+            {
+                if (directionalInput.x != -Mathf.Sign(controller.collisions.slopeNormal.x))
+                { // not jumping against max slope
+                    velocity.y = maxJumpVelocity * controller.collisions.slopeNormal.y;
+                    velocity.x = maxJumpVelocity * controller.collisions.slopeNormal.x;
+                }
+            }
+            else if (isSliding)
+            {
+                velocity.x = Mathf.Sign(velocity.x) * Mathf.Max(Mathf.Abs(velocity.x) * slideJumpBoost, slideJumpBoost * moveSpeed); // Boost horizontal velocity on jump
+                velocity.y = maxJumpVelocity * 0.8f;
+                StopSlide();
+
+            }
+            else
+            {
+                velocity.y = maxJumpVelocity;
+            }
+        }
 	}
 
 	public void OnJumpInputUp() {
 		if (velocity.y > minJumpVelocity) {
 			velocity.y = minJumpVelocity;
 		}
-	}
-		
+	}	
 
 	void HandleWallSliding() {
 		wallDirX = (controller.collisions.left) ? -1 : 1;
@@ -153,9 +254,112 @@ public class Player : MonoBehaviour {
 
 	}
 
-	void CalculateVelocity() {
-		float targetVelocityX = directionalInput.x * moveSpeed;
-		velocity.x = Mathf.SmoothDamp (velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below)?accelerationTimeGrounded:accelerationTimeAirborne);
-		velocity.y += gravity * Time.deltaTime;
-	}
+    void CalculateVelocity()
+    {
+        float targetVelocityX;
+        if (controller.collisions.below)
+        {
+            if (isSliding)
+            {
+                targetVelocityX = directionalInput.x * crawlSpeed;
+            }
+            else
+            {
+                targetVelocityX = directionalInput.x * moveSpeed;
+            }
+        }
+        else
+        {
+            targetVelocityX = directionalInput.x * moveSpeed;
+        }
+
+
+        // Apply sprint multiplier if sprinting
+        if (isSprinting && !isSliding)
+        {
+            float sprintFactor = Mathf.Lerp(1, sprintMultiplier, currentSprintTime / sprintRampUpTime);
+            targetVelocityX *= sprintFactor;
+        }
+        else if (isSliding)
+        {
+            float slideFactor = Mathf.Lerp(sprintMultiplier, slideMultiplier, currentSlideTime / slideRampDownTime);
+            targetVelocityX *= slideFactor;
+        }
+
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
+        velocity.y += gravity * Time.deltaTime;
+    }
+
+    void HandleSprinting()
+    {
+        if (isSprinting)
+        {
+            if (currentSprintTime < sprintRampUpTime)
+            {
+                currentSprintTime += Time.deltaTime;
+            }
+        }
+        else
+        {
+            currentSprintTime = 0f;
+        }
+    }
+
+    public void SetSprinting(bool sprinting)
+    {
+        isSprinting = sprinting;
+    }
+
+    public void StartSlide()
+    {
+        if (!controller.collisions.below || !isSprinting || Mathf.Abs(velocity.x) < moveSpeed) return;  // Require sprinting and ground contact
+
+        isSliding = true;
+        isCrawling = false;
+        transform.localScale = new Vector3(originalScale.x, originalScale.y * slideShrinkFactor, originalScale.z);  // Shrink player vertically
+        transform.position = new Vector3(transform.position.x, transform.position.y - (originalScale.y - transform.localScale.y) / 2, transform.position.z);
+        boxCollider.transform.position = transform.position;
+        boxCollider.bounds.size.Set(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        controller.UpdateRaycastOrigins();
+        
+    }
+
+    public void StopSlide()
+    {
+        isSliding = false;
+        isCrawling = false;
+        transform.localScale = originalScale; // Reset to original size
+        transform.position = new Vector3(transform.position.x, transform.position.y + (originalScale.y - transform.localScale.y) / 2, transform.position.z);
+        boxCollider.transform.position = transform.position;
+        boxCollider.bounds.size.Set(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        controller.UpdateRaycastOrigins();
+    }
+
+    void HandleSliding()
+    {
+        if (isSliding)
+        {
+            if (currentSlideTime < slideRampDownTime)
+            {
+                currentSlideTime += Time.deltaTime;
+            }
+        }
+        if (!isSliding)
+        {
+            currentSlideTime = 0f;
+            return;
+        }
+
+        if (directionalInput.y < 0)
+        {
+            isCrawling = true;
+            velocity.x = Mathf.SmoothDamp(velocity.x, crawlSpeed, ref velocityXSmoothing, accelerationTimeGrounded);
+        }
+
+        if (Mathf.Abs(velocity.x) < crawlSpeed && directionalInput.y >= 0)
+        {
+            StopSlide();
+        }
+    }
+
 }
