93ff0d5144d97b6297f79b42e491f16bb97f1752
Matt Low <matthewlowstx@gmail.com>
2025-03-22 21:29:45 -0600

    - villager script subclass of npc

---
+++ Assets/Scripts/NPCs/EE_Villager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EE_Villager : EE_NPC
+{
+	[Header("NPC Settings")]
+	public string villagerName;
+
+	[Header("Dialogue")]
+    public string speech;
+
+    // Start is called before the first frame update
+    void Start() { }
+
+    // Update is called once per frame
+    void Update() { }
+}
