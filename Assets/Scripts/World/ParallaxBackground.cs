1f661b95863af91def8b7952ce8744f4e133e252
lujarios <144175748+Lujarios@users.noreply.github.com>
2025-03-24 23:21:04 -0600

    Tutorial Design (BIG COMMIT)

--- Assets/Scripts/World/ParallaxBackground.cs
+++ Assets/Scripts/World/ParallaxBackground.cs
@@ -1,12 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteInEditMode]
+// [ExecuteInEditMode]
 public class ParallaxBackground : MonoBehaviour
 {
     public ParallaxCamera parallaxCamera;
     List<ParallaxLayer> parallaxLayers = new List<ParallaxLayer>();
+
+    private bool initialized = false;
 
     void Start()
     {
@@ -17,6 +20,37 @@ public class ParallaxBackground : MonoBehaviour
             parallaxCamera.onCameraTranslate += Move;
 
         SetLayers();
+
+        // Shift every child in every layer individually by their x distance from the camera * their parallax factor
+        if (!Application.isPlaying && !initialized) {
+
+            Console.WriteLine("ParallaxBackground Start");
+
+            float cameraX = Camera.main.transform.position.x;
+
+            for (int i = 0; i < parallaxLayers.Count; i++)
+            {
+                // Get all children in the layer
+                Transform[] children = parallaxLayers[i].GetComponentsInChildren<Transform>();
+
+                Console.WriteLine("Layer " + i + " has " + children.Length + " children");
+
+                // For each child, shift it by its x distance from the camera * the layer's parallax factor
+                foreach (Transform child in children)
+                {
+                    if (child == parallaxLayers[i].transform)
+                        continue;
+
+                    float parallaxFactor = parallaxLayers[i].parallaxFactor;
+                    Vector3 newPos = child.position;
+                    newPos.x += (cameraX - transform.position.x) * parallaxFactor;
+                    child.position = newPos;
+                }
+            }
+
+            initialized = true;
+        }
+
     }
 
     void SetLayers()
