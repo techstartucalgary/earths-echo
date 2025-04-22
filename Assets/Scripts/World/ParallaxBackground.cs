d82232ea8390fe7840f75da47190608af3795065
lujarios <144175748+Lujarios@users.noreply.github.com>
2025-04-21 23:42:27 -0600

    Grove & Tiger Boss

--- Assets/Scripts/World/ParallaxBackground.cs
+++ Assets/Scripts/World/ParallaxBackground.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// [ExecuteInEditMode]
+[ExecuteInEditMode]
 public class ParallaxBackground : MonoBehaviour
 {
     public ParallaxCamera parallaxCamera;
@@ -22,35 +22,36 @@ public class ParallaxBackground : MonoBehaviour
         SetLayers();
 
         // Shift every child in every layer individually by their x distance from the camera * their parallax factor
-        if (!Application.isPlaying && !initialized)
-        {
+        // if (!Application.isPlaying && !initialized)
+        // {
 
-            Console.WriteLine("ParallaxBackground Start");
+        //     Console.WriteLine("ParallaxBackground Start");
 
-            float cameraX = Camera.main.transform.position.x;
+        //     float cameraX = Camera.main.transform.position.x;
 
-            for (int i = 0; i < parallaxLayers.Count; i++)
-            {
-                // Get all children in the layer
-                Transform[] children = parallaxLayers[i].GetComponentsInChildren<Transform>();
+        //     for (int i = 0; i < parallaxLayers.Count; i++)
+        //     {
+        //         // Get all children in the layer
+        //         Transform[] children = parallaxLayers[i].GetComponentsInChildren<Transform>();
 
-                Console.WriteLine("Layer " + i + " has " + children.Length + " children");
+        //         Console.WriteLine("Layer " + i + " has " + children.Length + " children");
 
-                // For each child, shift it by its x distance from the camera * the layer's parallax factor
-                foreach (Transform child in children)
-                {
-                    if (child == parallaxLayers[i].transform)
-                        continue;
+        //         // For each child, shift it by its x distance from the camera * the layer's parallax factor
+        //         foreach (Transform child in children)
+        //         {
+        //             if (child == parallaxLayers[i].transform)
+        //                 continue;
 
-                    float parallaxFactor = parallaxLayers[i].parallaxFactor;
-                    Vector3 newPos = child.position;
-                    newPos.x += (cameraX - transform.position.x) * parallaxFactor;
-                    child.position = newPos;
-                }
-            }
+        //             float parallaxFactor = parallaxLayers[i].parallaxFactor;
+        //             Vector3 newPos = child.position;
+        //             newPos.x += (cameraX - transform.position.x) * parallaxFactor;
+        //             child.position = newPos;
+        //         }
+        //     }
 
-            initialized = true;
-        }
+        //     initialized = true;
+        // }
+
     }
 
     void SetLayers()
